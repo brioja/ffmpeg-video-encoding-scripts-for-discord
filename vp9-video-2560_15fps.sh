337d685bc5bf2e098caaf5be92e3217f107dd984
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2022-08-22 23:09:13 -0400

    cleanup command name

--- vp9-video-2560_15fps.sh
+++ vp9-video-2560_15fps.sh
@@ -16,8 +16,8 @@ fi
 ffmpeg -y -i "$1" -vf scale=2560:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 3000k \
   -minrate 1500k -maxrate 4350k -tile-columns 3 -g 240 -threads 16 \
   -quality good -crf 24 -c:v libvpx-vp9 -an \
-  -pass 1 -speed 4 "$1-compressed-2560.webm" && \
+  -pass 1 -speed 4 "$1-compressed-2560_15fps.webm" && \
 ffmpeg -y -i "$1" -vf scale=2560:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 3000k \
   -minrate 1500k -maxrate 4350k -tile-columns 3 -g 240 -threads 16 \
   -quality good -crf 24 -c:v libvpx-vp9 -an \
-  -pass 2 -speed 2 -y "$1-compressed-2560.webm"
+  -pass 2 -speed 2 -y "$1-compressed-2560_15fps.webm"
