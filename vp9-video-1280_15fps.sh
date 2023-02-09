74acd4567af04541a32a05b695bb3a2c31eb2126
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2023-02-08 20:26:50 -0500

    feat: add h264

--- vp9-video-1280_15fps.sh
+++ vp9-video-1280_15fps.sh
@@ -4,7 +4,7 @@ set -x
 
 #Good
 #MAX_VIDEO_SIZE="8000000"
-MAX_VIDEO_SIZE="8200000"
+MAX_VIDEO_SIZE="8000000"
 
 
 # Large
@@ -18,11 +18,11 @@ if [[ "$1" == "" ]]; then
 	exit
 fi
 
-ffmpeg -y -i "$1" -vf scale=1280:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 512k \
+ffmpeg -ss 00:00:02 -y -i "$1" -vf scale=1280:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 512k \
   -minrate 256k -maxrate 745k -tile-columns 2 -g 240 -threads 8 \
   -quality good -crf 32 -c:v libvpx-vp9 -an \
   -pass 1 -speed 4 "$1-compressed-1280_15fps.webm" && \
-ffmpeg -y -i "$1" -vf scale=1280:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 512k \
+ffmpeg -ss 00:00:02 -y -i "$1" -vf scale=1280:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 512k \
   -minrate 256k -maxrate 745k -tile-columns 2 -g 240 -threads 8 \
   -quality good -crf 32 -c:v libvpx-vp9 -an \
   -pass 2 -speed 2 -y "$1-compressed-1280_15fps.webm"
--- vp9-video-1920_15fps.sh
+++ vp9-video-1920_15fps.sh
@@ -4,7 +4,7 @@ set -x
 
 #Good
 #MAX_VIDEO_SIZE="8000000"
-MAX_VIDEO_SIZE="8200000"
+MAX_VIDEO_SIZE="7800000"
 
 
 # Large
@@ -18,11 +18,11 @@ if [[ "$1" == "" ]]; then
 	exit
 fi
 
-ffmpeg -y -i "$1" -vf scale=1920:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 900k \
+ffmpeg -ss 00:00:00 -y -i "$1" -vf scale=1920:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 900k \
   -minrate 450k -maxrate 1305k -tile-columns 2 -g 240 -threads 8 \
   -quality good -crf 31 -c:v libvpx-vp9 -an \
   -pass 1 -speed 4 "$1-compressed-1920_15fps.webm" && \
-ffmpeg -y -i "$1" -vf scale=1920:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 900k \
+ffmpeg -ss 00:00:00 -y -i "$1" -vf scale=1920:-1,fps=15 -fs ${MAX_VIDEO_SIZE} -b:v 900k \
   -minrate 450k -maxrate 1305k -tile-columns 3 -g 240 -threads 8 \
   -quality good -crf 31 -c:v libvpx-vp9 -an \
   -pass 2 -speed 2 -y "$1-compressed-1920_15fps.webm"
